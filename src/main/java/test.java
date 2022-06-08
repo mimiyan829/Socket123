a0f6022acba6b29809d7afeafdc840ad2a6141a9
caoxue <2064753679@qq.com>
2022-06-08 14:24:08 +0800

    11111

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+public class test {
+
+        public static void main(String[] args) {
+
+            System.out.println("测试");
+            System.out.println("master");
+            System.out.println("dev");
+        }
+
+}
